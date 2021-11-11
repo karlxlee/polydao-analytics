4bf6ba17cd65345ad53f05e8337703d9b58665eb
Karl Lee <poogooflupduck@gmail.com>
2021-11-11 15:06:16 +0000

    add api

--- webapp/pages/index.js
+++ webapp/pages/index.js
@@ -13,7 +13,7 @@ import {
   Text,
   Box,
 } from "@chakra-ui/react";
-
+import daoList from "../dao.json";
 export default function Home() {
   return (
     <>
@@ -30,4 +30,15 @@ export default function Home() {
       </Page>
     </>
   );
+}
+
+export async function getServerSideProps() {
+  let dao = Object.keys(daoList)[0];
+  console.log(dao);
+  const gov = await fetch(
+    "https://api.covalenthq.com/v1/1/events/address/0xc0Da02939E1441F497fd74F78cE7Decb17B66529/?starting-block=12115107&ending-block=12240004&key=ckey_5de627364f574dde947de753b67"
+  );
+  const res = await gov.json().then((r) => r.data);
+  console.log(res);
+  return { props: {} };
 }
